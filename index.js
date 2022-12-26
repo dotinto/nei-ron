c32c40876d70298ed78713d4c4dec9110c1113d5
corriolgamer@gmail.com <corriolgamer@gmail.com>
2022-12-26 10:27:55 +0200

    1

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
-const Discord = require('discord.js')
+const { REST, Routes, Discord } = require('discord.js');
 const client = new Discord.Client()
 
 var prefix = '.'
-var commands = []
 
-const fs = require('fs')
-fs.readFile('commands.json', (data, err) => {
-  if (err) {
-    return err
-  }
-  commands = JSON.parse(data)
-})
+const { clientId } = require('./config.json');
+const guildId = client.guilds.cache.id;
+const token = process.env.BOT_TOKEN;
 
-client.login(process.env.BOT_TOKEN)
+const commands = [];
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.login(token)
 
 client.on('ready', () => {
   console.log('Logged in as ' + client.user.tag)
-})
-
-client.on('message', message => {
-  
 })
